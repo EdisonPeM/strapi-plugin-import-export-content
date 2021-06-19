0a254ea6b7cf26e8fe0b868b783f5949a1eaec4b
Edison Peñuela <edisonpe961206@hotmail.com>
2021-06-19 00:06:21 -0500

    Add application/vnd.ms-excel type in formats, close issue #4

--- admin/src/constants/formats.js
+++ admin/src/constants/formats.js
@@ -1,5 +1,6 @@
 const FORMATS = [
   { name: "csv", mimeType: "text/csv", ext: ".csv" },
+  { name: "csv-excel", mimeType: "application/vnd.ms-excel", ext: ".csv" },
   { name: "json", mimeType: "application/json", ext: ".json" },
 ];
 
