6c49c454cf376bd1a7b8f3b3fb1e29f457577cf0
Edison Peñuela <edisonpe961206@hotmail.com>
2021-06-20 00:45:19 -0500

    Clean old Exporter

--- services/exporter/index.js
+++ services/exporter/index.js
@@ -1,25 +1,11 @@
-const PERMISSIONS = require("../constants/permissions");
-const ignoreFields = [
-  "id",
-  "created_at",
-  "created_by",
-  "updated_at",
-  "updated_by",
-  "published_at",
-];
+const PERMISSIONS = require("../../constants/permissions");
 
-function cleanFields(item) {
-  if (item === null || item === undefined) return;
-  Object.keys(item).forEach((itemKey) => {
-    if (ignoreFields.includes(itemKey)) return delete item[itemKey];
-    if (typeof item[itemKey] === "object") return cleanFields(item[itemKey]);
-  });
-
+function cleanFields(item, options) {
+  // CHANGE THIS TO FILTER WITH OPTIONS
   return item;
 }
 
-async function getAll(uid, ctx) {
-  const { userAbility } = ctx.state;
+async function getData(uid, options, userAbility) {
   const permissionsManager =
     strapi.admin.services.permission.createPermissionsManager({
       ability: userAbility,
@@ -33,9 +19,11 @@ async function getAll(uid, ctx) {
     { model: uid }
   );
 
-  return Array.isArray(items) ? items.map(cleanFields) : cleanFields(items);
+  return Array.isArray(items)
+    ? items.map((item) => cleanFields(item, options))
+    : cleanFields(items, options);
 }
 
 module.exports = {
-  getAll,
+  getData,
 };
