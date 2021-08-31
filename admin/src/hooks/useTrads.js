767ea34f0d084001da609cb60667501670058803
Edison Peñuela <edisonpe961206@hotmail.com>
2021-08-31 18:16:41 -0500

    Add Export Page Translations

--- admin/src/hooks/useTrads.js
+++ admin/src/hooks/useTrads.js
@@ -2,8 +2,7 @@ import { useGlobalContext } from "strapi-helper-plugin";
 import pluginId from "../pluginId";
 
 export default function useTrads() {
-  const { formatMessage, ...oters } = useGlobalContext();
-  console.log(oters);
+  const { formatMessage } = useGlobalContext();
   const format = (id, values = {}) =>
     formatMessage({ id: `${pluginId}.${id}`, values });
 
