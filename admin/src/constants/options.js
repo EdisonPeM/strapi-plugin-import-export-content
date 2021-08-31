9be51c89f6e29fbc7388546cf2dc2f281187b2d3
Edison Peñuela <edisonpe961206@hotmail.com>
2021-08-31 18:58:44 -0500

    Add OptionsExport Translations

--- admin/src/constants/options.js
+++ admin/src/constants/options.js
@@ -2,29 +2,25 @@ const BASE_OPTIONS = [
   {
     name: "medias",
     type: "select",
-    label: "Select how medias will be exported.",
     optionalValues: ["none", "ids", "url", "without-formats", "full"],
     defaultValue: "full",
   },
   {
     name: "relations",
     type: "select",
-    label: "Select how relation will be exported.",
     optionalValues: ["none", "ids", "full"],
     defaultValue: "full",
   },
   {
     name: "ids",
-    label: "Remove Ids",
     type: "boolean",
     defaultValue: false,
   },
   {
     name: "timestamp",
-    label: "Remove TimeStamps",
     type: "boolean",
     defaultValue: false,
-  }
+  },
 ];
 
 export default BASE_OPTIONS;
