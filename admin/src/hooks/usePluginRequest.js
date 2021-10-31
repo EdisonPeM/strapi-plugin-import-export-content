dde004209872e3ebacf3d4c4ff596a853573d9e6
Edison Peñuela <edisonpe961206@hotmail.com>
2021-10-31 14:35:58 -0500

    Add custom hook to request to the plugin server

---
+++ admin/src/hooks/usePluginRequest.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { request } from "strapi-helper-plugin";
+import pluginId from "../pluginId";
+
+function usePluginRequest() {
+  const [state, setState] = useState({
+    data: null,
+    loading: false,
+    error: null,
+  });
+
+  const sendData = async ({ url, body = {} }) => {
+    try {
+      setState((prevState) => ({ ...prevState, loading: true }));
+      const { data } = await request(`/${pluginId}/${url}`, {
+        method: "POST",
+        body,
+      });
+
+      setState({ data, loading: false, error: null });
+      return data;
+    } catch (error) {
+      console.error(error);
+      setState({ data: null, loading: false, error });
+      throw error;
+    }
+  };
+
+  return { ...state, sendData };
+}
+
+export default usePluginRequest;
