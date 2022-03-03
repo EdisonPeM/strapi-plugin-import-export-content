9dafba39b4d3016417c73b3c09c043d51643566a
Finley Garton <finley@fogldn.com>
2022-03-03 15:20:30 +0000

    removed unused code in mime extension

--- services/utils/mimeExtension.js
+++ services/utils/mimeExtension.js
@@ -2,12 +2,6 @@ function mimeExtension() {
   switch (type) {
     case "text/csv":
     case "application/vnd.ms-excel": {
-      const mappedItems = toArray(items);
-      const headers = getFieldsFromItems(mappedItems);
-      const data = mappedItems
-        .map((item) => jsonToCsv(item, headers))
-        .join("\n");
-
       return 'csv';
     }
     case "application/json": {
